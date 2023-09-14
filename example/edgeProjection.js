7bedf2a47817e9e94dd77c028efb2651c2b00857
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-09-14 23:46:41 +0200

    Clean up example

--- example/edgeProjection.js
+++ example/edgeProjection.js
@@ -1,4 +1,17 @@
-import * as THREE from 'three';
+import {
+	Box3,
+	WebGLRenderer,
+	Scene,
+	DirectionalLight,
+	AmbientLight,
+	Group,
+	MeshStandardMaterial,
+	MeshBasicMaterial,
+	BufferGeometry,
+	LineSegments,
+	LineBasicMaterial,
+	PerspectiveCamera,
+} from 'three';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -20,7 +33,7 @@ const params = {
 		group.position.set( 0, 0, 0 );
 		group.updateMatrixWorld( true );
 
-		const box = new THREE.Box3();
+		const box = new Box3();
 		box.setFromObject( model, true );
 		box.getCenter( group.position ).multiplyScalar( - 1 );
 		group.position.y = Math.max( 0, - box.min.y ) + 1;
@@ -33,7 +46,7 @@ const params = {
 	},
 };
 
-const ANGLE_THRESHOLD = 1;
+const ANGLE_THRESHOLD = 50;
 let renderer, camera, scene, gui, controls;
 let lines, model, projection, group, shadedWhiteModel, whiteModel;
 let outputContainer;
@@ -49,25 +62,25 @@ async function init() {
 	const bgColor = 0xeeeeee;
 
 	// renderer setup
-	renderer = new THREE.WebGLRenderer();
+	renderer = new WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setClearColor( bgColor, 1 );
 	document.body.appendChild( renderer.domElement );
 
 	// scene setup
-	scene = new THREE.Scene();
+	scene = new Scene();
 
 	// lights
-	const light = new THREE.DirectionalLight( 0xffffff, 3.5 );
+	const light = new DirectionalLight( 0xffffff, 3.5 );
 	light.position.set( 1, 2, 3 );
 	scene.add( light );
 
-	const ambientLight = new THREE.AmbientLight( 0xb0bec5, 0.5 );
+	const ambientLight = new AmbientLight( 0xb0bec5, 0.5 );
 	scene.add( ambientLight );
 
 	// load model
-	group = new THREE.Group();
+	group = new Group();
 	scene.add( group );
 
 	const gltf = await new GLTFLoader()
@@ -75,7 +88,7 @@ async function init() {
 		.loadAsync( 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/nasa-m2020/Perseverance.glb' );
 	model = gltf.scene;
 
-	const whiteMaterial = new THREE.MeshStandardMaterial( {
+	const whiteMaterial = new MeshStandardMaterial( {
 		polygonOffset: true,
 		polygonOffsetFactor: 1,
 		polygonOffsetUnits: 1,
@@ -91,7 +104,7 @@ async function init() {
 
 	} );
 
-	const whiteBasicMaterial = new THREE.MeshBasicMaterial( {
+	const whiteBasicMaterial = new MeshBasicMaterial( {
 		polygonOffset: true,
 		polygonOffsetFactor: 1,
 		polygonOffsetUnits: 1,
@@ -110,24 +123,24 @@ async function init() {
 	group.updateMatrixWorld( true );
 
 	// center model
-	const box = new THREE.Box3();
+	const box = new Box3();
 	box.setFromObject( model, true );
 	box.getCenter( group.position ).multiplyScalar( - 1 );
 	group.position.y = Math.max( 0, - box.min.y ) + 1;
 	group.add( model, shadedWhiteModel, whiteModel );
 
 	// generate geometry line segments
-	lines = new THREE.Group();
+	lines = new Group();
 	model.traverse( c => {
 
 		if ( c.geometry ) {
 
 			const edges = generateEdges( c.geometry, undefined, ANGLE_THRESHOLD );
 			const points = edges.flatMap( line => [ line.start, line.end ] );
-			const geom = new THREE.BufferGeometry();
+			const geom = new BufferGeometry();
 			geom.setFromPoints( points );
 
-			const geomLines = new THREE.LineSegments( geom, new THREE.LineBasicMaterial( { color: 0x030303 } ) );
+			const geomLines = new LineSegments( geom, new LineBasicMaterial( { color: 0x030303 } ) );
 			geomLines.position.copy( c.position );
 			geomLines.quaternion.copy( c.quaternion );
 			geomLines.scale.copy( c.scale );
@@ -139,11 +152,11 @@ async function init() {
 	group.add( lines );
 
 	// create projection display mesh
-	projection = new THREE.LineSegments( new THREE.BufferGeometry(), new THREE.LineBasicMaterial( { color: 0x030303 } ) );
+	projection = new LineSegments( new BufferGeometry(), new LineBasicMaterial( { color: 0x030303 } ) );
 	scene.add( projection );
 
 	// camera setup
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 50 );
+	camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 50 );
 	camera.position.setScalar( 3.5 );
 	camera.updateProjectionMatrix();
 
