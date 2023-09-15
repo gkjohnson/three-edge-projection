cbd01e834c5194098d466d55b728f8db9633bba7
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-09-16 00:27:38 +0200

    Update triangle tim

--- src/utils/trimBeneathTriPlane.js
+++ src/utils/trimBeneathTriPlane.js
@@ -1,21 +1,12 @@
-import { Vector3 } from 'three';
-import { getPlaneYAtPoint } from './planeUtils.js';
+import { Plane, Vector3 } from 'three';
 import { isYProjectedTriangleDegenerate } from './triangleLineUtils.js';
 
 const EPSILON = 1e-16;
-const _lineDirection = /* @__PURE__ */ new Vector3();
+const UP_VECTOR = /* @__PURE__ */ new Vector3( 0, 1, 0 );
+const _plane = /* @__PURE__ */ new Plane();
 const _planeHit = /* @__PURE__ */ new Vector3();
-const _planePoint = /* @__PURE__ */ new Vector3();
+const _lineDirection = /* @__PURE__ */ new Vector3();
 export function trimToBeneathTriPlane( tri, line, lineTarget ) {
-
-	const { plane } = tri;
-	lineTarget.copy( line );
-
-	if ( tri.needsUpdate ) {
-
-		tri.update();
-
-	}
 
 	// if the triangle is insignificant then skip it
 	if ( isYProjectedTriangleDegenerate( tri ) ) {
@@ -24,52 +15,60 @@ export function trimToBeneathTriPlane( tri, line, lineTarget ) {
 
 	}
 
-	// if the line and plane are coplanar then return that we can't trim
-	line.delta( _lineDirection );
+	// update triangle if needed
+	if ( tri.needsUpdate ) {
 
-	const areCoplanar = Math.abs( plane.normal.dot( _lineDirection ) ) < EPSILON;
-	if ( areCoplanar ) {
+		tri.update();
+
+	}
+
+	// if the plane is not facing up then flip the direction
+	_plane.copy( tri.plane );
+	if ( _plane.normal.dot( UP_VECTOR ) < 0 ) {
+
+		_plane.normal.multiplyScalar( - 1 );
+		_plane.constant *= - 1;
+
+	}
+
+	// if the line and plane are coplanar then return that we can't trim
+	line.delta( _lineDirection ).normalize();
+	if ( Math.abs( _plane.normal.dot( _lineDirection ) ) < EPSILON ) {
 
 		return false;
 
 	}
 
-	// if the line does intersect the plane then trim
-	const doesLineIntersect = plane.intersectLine( line, _planeHit );
-	if ( doesLineIntersect ) {
+	// find the point that's below the plane. If both points are below the plane
+	// then we assume we're dealing with floating point error
+	const isStartBelow = _plane.distanceToPoint( line.start ) < 0;
+	const isEndBelow = _plane.distanceToPoint( line.end ) < 0;
+	if ( isStartBelow && isEndBelow ) {
 
-		const { start, end } = lineTarget;
-
-		// test the line side with the largest segment extending beyond the plane
-		let testPoint, otherPoint;
-		if ( start.distanceToSquared( _planeHit ) > end.distanceToSquared( _planeHit ) ) {
-
-			testPoint = start;
-			otherPoint = end;
-
-		} else {
-
-			testPoint = end;
-			otherPoint = start;
-
-		}
-
-		// get the center point of the line segment and the plane hit
-		getPlaneYAtPoint( tri.plane, testPoint, _planePoint );
-
-		// adjust the appropriate line point align with the plane hit point
-		const isAbove = testPoint.y > _planePoint.y;
-		if ( isAbove ) {
-
-			testPoint.copy( _planeHit );
-
-		} else {
-
-			otherPoint.copy( _planeHit );
-
-		}
-
+		// if the whole line is below then just copy that
+		lineTarget.copy( line );
 		return true;
+
+	} else if ( ! isStartBelow && ! isEndBelow ) {
+
+		// if it's wholly above then skip it
+		return false;
+
+	} else if ( _plane.intersectLine( line, _planeHit ) ) {
+
+		if ( isStartBelow ) {
+
+			lineTarget.start.copy( line.start );
+			lineTarget.end.copy( _planeHit );
+			return true;
+
+		} else if ( isEndBelow ) {
+
+			lineTarget.end.copy( line.end );
+			lineTarget.start.copy( _planeHit );
+			return true;
+
+		}
 
 	}
 
