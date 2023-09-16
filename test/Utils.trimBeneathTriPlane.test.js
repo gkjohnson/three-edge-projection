c65f90f4c2baf8cef78d485f753fc50e0148da04
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-09-16 13:53:27 +0200

    Add more tests

--- test/Utils.trimBeneathTriPlane.test.js
+++ test/Utils.trimBeneathTriPlane.test.js
@@ -44,4 +44,39 @@ describe( 'trimBeneathTriPlane', () => {
 
 	} );
 
+	it( 'should return the whole line is completely beneath the triangle.', () => {
+
+		const triangle = new ExtendedTriangle(
+			new Vector3( - 1, 0, 0 ),
+			new Vector3( 0, 0, 1 ),
+			new Vector3( 1, 0, 0 ),
+		);
+
+		let line, target;
+		target = new Line3();
+		line = new Line3( new Vector3( 0, - 2, 0.5 ), new Vector3( 0, - 0.5, 0.5 ) );
+
+		expect( trimToBeneathTriPlane( triangle, line, target ) ).toBe( true );
+		expect( target.distance() ).toBe( 1.5 );
+		expect( target ).toEqual( line );
+
+	} );
+
+
+	it( 'should not return anything if the whole line is completely above the triangle.', () => {
+
+		const triangle = new ExtendedTriangle(
+			new Vector3( - 1, 0, 0 ),
+			new Vector3( 0, 0, 1 ),
+			new Vector3( 1, 0, 0 ),
+		);
+
+		let line, target;
+		target = new Line3();
+		line = new Line3( new Vector3( 0, 2, 0.5 ), new Vector3( 0, 0.5, 0.5 ) );
+
+		expect( trimToBeneathTriPlane( triangle, line, target ) ).toBe( false );
+
+	} );
+
 } );
