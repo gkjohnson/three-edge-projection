5fb4368990806d0efab688ed4f47dccad28d65bc
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-11-20 12:52:26 +0900

    Improve intersecting edge perf

--- src/utils/generateIntersectionEdges.js
+++ src/utils/generateIntersectionEdges.js
@@ -1,95 +1,60 @@
-import { Line3 } from 'three';
-import { ExtendedTriangle } from 'three-mesh-bvh';
+import { Line3, Matrix4 } from 'three';
 import { isLineTriangleEdge } from './triangleLineUtils.js';
 
+// TODO: How can we add support for "iterationTime"?
+// TODO: Fix byte offsets in three-mesh-bvh (check shapecast, bvhcast)
+
 const OFFSET_EPSILON = 1e-6;
-const _tri = new ExtendedTriangle();
 const _line = new Line3();
+const _mat = new Matrix4();
 export function* generateIntersectionEdges( bvh, target = [], options = {} ) {
 
 	const {
 		iterationTime = 30,
 	} = options;
 
-	const geometry = bvh.geometry;
-	const index = geometry.index;
-	const posAttr = geometry.attributes.position;
-	const vertCount = index ? index.count : posAttr;
+	bvh.bvhcast( bvh, _mat, {
+		intersectsTriangles: ( tri1, tri2 ) => {
 
-	let time = performance.now();
-	for ( let i = 0; i < vertCount; i += 3 ) {
+			if ( tri1.equals( tri2 ) ) {
 
-		const delta = performance.now() - time;
-		if ( delta > iterationTime ) {
+				return;
 
-			yield;
-			time = performance.now();
+			}
+
+			if ( tri1.needsUpdate ) {
+
+				tri1.update();
+
+			}
+
+			if ( tri2.needsUpdate ) {
+
+				tri2.update();
+
+			}
+
+			if ( Math.abs( tri1.plane.normal.dot( tri2.plane.normal ) ) > 1 - 1e-6 ) {
+
+				return false;
+
+			}
+
+			if (
+				tri1.intersectsTriangle( tri2, _line, true ) &&
+				! isLineTriangleEdge( tri1, _line ) &&
+				! isLineTriangleEdge( tri2, _line )
+			) {
+
+				_line.start.y += OFFSET_EPSILON;
+				_line.end.y += OFFSET_EPSILON;
+				target.push( _line.clone() );
+
+			}
 
 		}
 
-		let i0 = i + 0;
-		let i1 = i + 1;
-		let i2 = i + 2;
-		if ( index ) {
-
-			i0 = index.getX( i0 );
-			i1 = index.getX( i1 );
-			i2 = index.getX( i2 );
-
-		}
-
-		const { a, b, c } = _tri;
-		a.fromBufferAttribute( posAttr, i0 );
-		b.fromBufferAttribute( posAttr, i1 );
-		c.fromBufferAttribute( posAttr, i2 );
-		_tri.needsUpdate = true;
-		_tri.update();
-
-		bvh.shapecast( {
-
-			intersectsBounds: box => {
-
-				return box.intersectsTriangle( _tri );
-
-			},
-
-			intersectsTriangle: tri2 => {
-
-				if ( _tri.equals( tri2 ) ) {
-
-					return false;
-
-				}
-
-				if ( tri2.needsUpdate ) {
-
-					tri2.update();
-
-				}
-
-				if ( Math.abs( _tri.plane.normal.dot( tri2.plane.normal ) ) > 1 - 1e-6 ) {
-
-					return false;
-
-				}
-
-				if (
-					_tri.intersectsTriangle( tri2, _line, true ) &&
-					! isLineTriangleEdge( _tri, _line ) &&
-					! isLineTriangleEdge( tri2, _line )
-				) {
-
-					_line.start.y += OFFSET_EPSILON;
-					_line.end.y += OFFSET_EPSILON;
-					target.push( _line.clone() );
-
-				}
-
-			},
-
-		} );
-
-	}
+	} );
 
 	return target;
 
