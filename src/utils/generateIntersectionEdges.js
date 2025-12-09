06623b80ba6cebf3481503f3dac74787fae8e196
Garrett Johnson <garrett.kjohnson@gmail.com>
2025-12-09 09:28:45 +0900

    Handle another corner case

--- src/utils/generateIntersectionEdges.js
+++ src/utils/generateIntersectionEdges.js
@@ -3,11 +3,17 @@ import { isLineTriangleEdge } from './triangleLineUtils.js';
 
 // TODO: How can we add support for "iterationTime"?
 
-const _line = new Line3();
+const _line = /* @__PURE__ */ new Line3();
 export function generateIntersectionEdges( bvhA, bvhB, matrixBToA, target = [] ) {
 
 	bvhA.bvhcast( bvhB, matrixBToA, {
 		intersectsTriangles: ( tri1, tri2 ) => {
+
+			if ( areTrianglesOnEdge( tri1, tri2 ) ) {
+
+				return false;
+
+			}
 
 			if ( tri1.needsUpdate ) {
 
@@ -27,10 +33,6 @@ export function generateIntersectionEdges( bvhA, bvhB, matrixBToA, target = [] )
 
 			}
 
-			// TODO: Is this correct? Should we just ignore lines if the edge is on the edge
-			// of _both_ triangles?
-			// TODO: Consider checking if the triangles are connected at the edges first as
-			// an optimization?
 			if (
 				tri1.intersectsTriangle( tri2, _line, true ) &&
 				! isLineTriangleEdge( tri1, _line ) &&
@@ -46,5 +48,35 @@ export function generateIntersectionEdges( bvhA, bvhB, matrixBToA, target = [] )
 	} );
 
 	return target;
+
+}
+
+function areVectorsEqual( a, b ) {
+
+	return a.distanceTo( b ) < 1e-10;
+
+}
+
+function areTrianglesOnEdge( t1, t2 ) {
+
+	const indices = [ 'a', 'b', 'c' ];
+	let tot = 0;
+	for ( let i = 0; i < 3; i ++ ) {
+
+		for ( let j = 0; j < 3; j ++ ) {
+
+			const v0 = t1[ indices[ i ] ];
+			const v1 = t2[ indices[ j ] ];
+			if ( areVectorsEqual( v0, v1 ) ) {
+
+				tot ++;
+
+			}
+
+		}
+
+	}
+
+	return tot >= 2;
 
 }
