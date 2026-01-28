70d3a3101dc96880feb36cedb5aa70ad79d5f5fc
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-01-28 20:03:54 +0900

    Add edge-split strategy

--- src/ProjectionGenerator.js
+++ src/ProjectionGenerator.js
@@ -4,6 +4,11 @@ import {
 	BufferAttribute,
 	Mesh,
 	Scene,
+	Line3,
+	Box3,
+	Raycaster,
+	MathUtils,
+	Points,
 } from 'three';
 import { MeshBVH, SAH } from 'three-mesh-bvh';
 import { isYProjectedLineDegenerate } from './utils/triangleLineUtils.js';
@@ -16,6 +21,16 @@ import { getAllMeshes } from './utils/getAllMeshes.js';
 // these shared variables are not used across "yield" boundaries in the
 // generator so there's no risk of overwriting another tasks data
 const UP_VECTOR = /* @__PURE__ */ new Vector3( 0, 1, 0 );
+
+const _raycaster = /* @__PURE__ */ new Raycaster();
+const _line = /* @__PURE__ */ new Line3();
+const _line0 = /* @__PURE__ */ new Line3();
+const _line1 = /* @__PURE__ */ new Line3();
+const _box = /* @__PURE__ */ new Box3();
+const _point0 = /* @__PURE__ */ new Vector3();
+const _point1 = /* @__PURE__ */ new Vector3();
+const _dir0 = /* @__PURE__ */ new Vector3();
+const _dir1 = /* @__PURE__ */ new Vector3();
 
 function toLineGeometry( edges ) {
 
@@ -49,6 +64,7 @@ class ProjectedEdgeCollector {
 		this.visibleEdges = [];
 		this.hiddenEdges = [];
 		this.iterationTime = 30;
+		this.intersectionApproach = false;
 
 	}
 
@@ -119,43 +135,182 @@ class ProjectedEdgeCollector {
 
 		// construct bvh
 		const edgesBvh = new LineObjectsBVH( edges, { maxLeafSize: 2, strategy: SAH } );
-		time = performance.now();
-		for ( let m = 0; m < meshes.length; m ++ ) {
 
-			if ( performance.now() - time > iterationTime ) {
+		if ( this.intersectionApproach ) {
 
-				if ( options.onProgress ) {
+			// TODO: use objects bvh for scene to accelerate raycasts
+			// TODO: book keep the splits separately in order to avoid duplicate clipping
 
-					options.onProgress( m, meshes.length );
+			const splits = [];
+			const hits = [];
+
+			for ( let i0 = 0, l = edgesBvh.lines.length; i0 < l; i0 ++ ) {
+
+				// get the projected line
+				const e0 = edgesBvh.lines[ i0 ];
+				_line0.copy( e0 );
+				_line0.start.y = 0;
+				_line0.end.y = 0;
+
+				// get the line direction
+				_line0.delta( _dir0 ).normalize();
+
+				// get the line bounds
+				_box.makeEmpty();
+				_box.expandByPoint( e0.start );
+				_box.expandByPoint( e0.end );
+
+				// expand the size
+				_box.max.y = 1e5;
+				_box.min.y = - 1e5;
+
+				// reset the splits and
+				splits.length = 0;
+				edgesBvh.shapecast( {
+
+					intersectsBounds( b ) {
+
+						return _box.intersectsBox( b );
+
+					},
+
+					intersectsRange( offset, count ) {
+
+						for ( let i1 = offset, l = offset + count; i1 < l; i1 ++ ) {
+
+							if ( i0 === i1 ) {
+
+								continue;
+
+							}
+
+							// get the projected line
+							const e1 = edgesBvh.lines[ i1 ];
+							_line1.copy( e1 );
+							_line1.start.y = 0;
+							_line1.end.y = 0;
+
+							// get projected direction
+							_line1.delta( _dir1 ).normalize();
+
+							// early out if parallel
+							if ( Math.abs( _dir0.dot( _dir1 ) ) > 1 - 1e-5 ) {
+
+								continue;
+
+							}
+
+							// get the intersection point
+							const dist = _line0.distanceSqToLine3( _line1, _point0, _point1 );
+							if ( dist < 1e-5 ) {
+
+								splits.push( _line0.closestPointToPointParameter( _point1 ) );
+
+							}
+
+						}
+
+					}
+
+				} );
+
+				// sort the splits
+				splits.push( 0, 1 );
+				splits.sort( ( a, b ) => a - b );
+
+				// iterate over splits
+				_raycaster.firstHitOnly = true;
+				for ( let i = 0; i < splits.length - 1; i ++ ) {
+
+					const s0 = splits[ i ];
+					const s1 = splits[ i + 1 ];
+					if ( s0 === s1 ) {
+
+						continue;
+
+					}
+
+					// set up the raycaster to project check the middle of the line
+					const middle = ( s0 + s1 ) / 2;
+					e0.at( middle, _raycaster.ray.origin );
+					_raycaster.ray.origin.y += 1e4;
+					_raycaster.far = 1e4;
+					_raycaster.ray.direction.set( 0, - 1, 0 );
+
+					// perform the raycasting check
+					let visible = true;
+					for ( let m = 0, lm = meshes.length; m < lm; m ++ ) {
+
+						const mesh = meshes[ m ];
+						const bvh = bvhs.get( mesh.geometry );
+						hits.length = 0;
+						bvh.raycastObject3D( mesh, _raycaster, hits );
+						if ( hits.length > 0 ) {
+
+							visible = false;
+
+						}
+
+					}
+
+					// if it's visible then save it
+					if ( visible ) {
+
+						e0.at( s0, _line.start );
+						e0.at( s1, _line.end );
+
+						visibleEdges.push( new Float32Array( [
+							_line.start.x, 0, _line.start.z,
+							_line.end.x, 0, _line.end.z,
+						] ) );
+
+					}
 
 				}
 
-				yield;
-				time = performance.now();
+			}
+
+		} else {
+
+			time = performance.now();
+			for ( let m = 0; m < meshes.length; m ++ ) {
+
+				if ( performance.now() - time > iterationTime ) {
+
+					if ( options.onProgress ) {
+
+						options.onProgress( m, meshes.length );
+
+					}
+
+					yield;
+					time = performance.now();
+
+				}
+
+				// use bvhcast to compare all edges against all meshes
+				const mesh = meshes[ m ];
+				bvhcastEdges( edgesBvh, bvhs.get( mesh.geometry ), mesh, hiddenOverlapMap );
 
 			}
 
-			// use bvhcast to compare all edges against all meshes
-			const mesh = meshes[ m ];
-			bvhcastEdges( edgesBvh, bvhs.get( mesh.geometry ), mesh, hiddenOverlapMap );
+			// construct the projections
+			for ( let i = 0; i < edges.length; i ++ ) {
 
-		}
+				if ( performance.now() - time > iterationTime ) {
 
-		// construct the projections
-		for ( let i = 0; i < edges.length; i ++ ) {
+					yield;
+					time = performance.now();
 
-			if ( performance.now() - time > iterationTime ) {
+				}
 
-				yield;
-				time = performance.now();
+				// convert the overlap points to proper lines
+				const line = edges[ i ];
+				const hiddenOverlaps = hiddenOverlapMap[ i ];
+				overlapsToLines( line, hiddenOverlaps, false, visibleEdges );
+				overlapsToLines( line, hiddenOverlaps, true, hiddenEdges );
 
 			}
-
-			// convert the overlap points to proper lines
-			const line = edges[ i ];
-			const hiddenOverlaps = hiddenOverlapMap[ i ];
-			overlapsToLines( line, hiddenOverlaps, false, visibleEdges );
-			overlapsToLines( line, hiddenOverlaps, true, hiddenEdges );
 
 		}
 
@@ -224,6 +379,8 @@ export class ProjectionGenerator {
 		}
 
 		if ( visibilityCuller ) {
+
+			onProgress( 'Culling non-visible geometry' );
 
 			let finished = false;
 			visibilityCuller.cull( scene ).then( res => {
